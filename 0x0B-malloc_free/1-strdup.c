9c19702639cc1321765e45ca76b56a84002bea4d
Codemermoz <kouakoumermoz033@gmail.com>
2023-01-10 07:50:27 -0800

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,28 +1,48 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * _strdup - returns a pointer to a newly allocated space in memory.
- * @str: string.
- *
- * Return: pointer of an array of chars
+ * _strlen - returns the length of a string
+ * @s: string s
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s)
+	{
+		s++;
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: string to be copied
+ * Return: copied string
  */
 char *_strdup(char *str)
 {
-	char *strout;
-	unsigned int i, j;
 
-	if (str == NULL)
+	char *copy, *_copy;
+
+	if (!str)
 		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
-
-	strout = (char *)malloc(sizeof(char) * (i + 1));
-
-	if (strout == NULL)
+	copy = malloc((_strlen(str) + 1) * sizeof(char));
+	/*incase there is insufficent memory*/
+	if (!copy)
 		return (NULL);
+	_copy = copy;
 
-	for (j = 0; j <= i; j++)
-		strout[j] = str[j];
-
-	return (strout);
+	while (*str)
+	{
+		*_copy = *str;
+		str++;
+		_copy++;
+	}
+	*_copy = '\0';
+	return (copy);
 }
